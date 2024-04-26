7bf4ef6bd40487e5e8e716927ac14652808b0065
Alex Heeren <eaheeren@gmail.com>
2024-04-26 15:16:41 -0400

    added clang-format options and some options for neovim.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -23,3 +23,21 @@ vim.opt.colorcolumn = "80"
 vim.opt.tags = "./tags;,"
 
 vim.opt.clipboard = "unnamedplus"
+
+-- Function to setup .ino files as C++
+local function setup_arduino_files()
+  -- Set filetype to cpp for .ino files
+  vim.api.nvim_command("set filetype=cpp")
+  -- Set the comment string for C++ files
+  vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+end
+
+-- Create an autocommand group for Arduino setup
+vim.api.nvim_create_augroup("ArduinoSetup", { clear = true })
+
+-- Autocommand to run setup function for .ino files
+vim.api.nvim_create_autocmd("BufRead,BufNewFile", {
+  pattern = "*.ino",
+  group = "ArduinoSetup",
+  callback = setup_arduino_files,
+})
