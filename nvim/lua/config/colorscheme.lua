974faba20c5f23c28a2596fd02fb41f70d408f72
Alex Heeren <eaheeren@gmail.com>
2024-05-14 01:13:35 -0400

    Changed default colorscheme to elflord and remapped dd to not copy to register.

---
+++ nvim/lua/config/colorscheme.lua
@@ -0,0 +1,8 @@
+-- -- Set the colorscheme to tokyonight using a protected call
+-- -- in case it isn't installed
+-- local status, _ = pcall(vim.cmd, "colorscheme elflord")
+-- if not status then 
+--     print("Colorscheme not found!") -- Print an error message if the colorscheme is not installed
+-- return
+-- end
+vim.cmd "colorscheme elflord"
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Setup key mappings in normal and visual modes to delete text without saving it to any register
+local function setup_mappings()
+    -- Normal mode mappings
+    vim.api.nvim_set_keymap('n', 'd', '"_d', {noremap = true, silent = true})
+    vim.api.nvim_set_keymap('n', 'dd', '"_dd', {noremap = true, silent = true})
+
+    -- Visual mode mappings
+    vim.api.nvim_set_keymap('v', 'd', '"_d', {noremap = true, silent = true})
+    vim.api.nvim_set_keymap('v', 'dd', '"_dd', {noremap = true, silent = true})
+end
+
+-- Call the function to apply the mappings
+setup_mappings()
