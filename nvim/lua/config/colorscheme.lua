293583ad88b356251ecde465cb507e7cfcf3760a
Alex Heeren <eaheeren@gmail.com>
2024-05-14 01:20:49 -0400

    This time the colors actually work.

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,8 +1,9 @@
 -- -- Set the colorscheme to tokyonight using a protected call
 -- -- in case it isn't installed
 -- local status, _ = pcall(vim.cmd, "colorscheme elflord")
--- if not status then 
+-- if not status then
 --     print("Colorscheme not found!") -- Print an error message if the colorscheme is not installed
 -- return
 -- end
-vim.cmd "colorscheme elflord"
+
+vim.cmd("colorscheme elflord")
